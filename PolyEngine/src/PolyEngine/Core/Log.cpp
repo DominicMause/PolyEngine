f622030eb8af4ae595f94efbfb4a202d478fea8f
Dominic Mause <dominicmause@web.de>
2024-03-30 23:27:10 +0100

    Added Log file

--- PolyEngine/src/PolyEngine/Core/Log.cpp
+++ PolyEngine/src/PolyEngine/Core/Log.cpp
@@ -1,7 +1,8 @@
 #include "pepch.h"
 #include "Log.h"
 
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace PolyEngine
 {
@@ -10,11 +11,21 @@ namespace PolyEngine
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("POLY ENGINE");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("PolyEngine.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("PolyEngine", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
-		 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
