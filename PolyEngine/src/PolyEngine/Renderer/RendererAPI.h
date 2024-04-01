e47db2895adb9edd1c343c69b25828192a135036
Dominic Mause <dominicmause@web.de>
2024-04-01 20:15:23 +0200

    Update RendererAPI.h

--- PolyEngine/src/PolyEngine/Renderer/RendererAPI.h
+++ PolyEngine/src/PolyEngine/Renderer/RendererAPI.h
@@ -24,7 +24,7 @@ namespace PolyEngine
 
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) = 0;
 
-		inline static API GetAPI() { return s_API; }
+		static API GetAPI() { return s_API; }
 		static Scope<RendererAPI> Create();
 		
 	private:
